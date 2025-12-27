db2403f9c6aadbf2dfcf0f4d184891c3c7ed9a20
githead927 <githubb2024@gmail.com>
2025-12-27 13:42:01 +0530

    H&H

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,9 +1,20 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function Hero() {
   return (
-    <div>
-      
+    <div className='w-full relative h-screen overflow-hidden'>
+     <img src="/1.gif" alt="hero" width={1000} height={1000} 
+     className='w-full h-full object-cover absolute inset-0 '/>
+     <div className='absolute w-full flex flex-col items-center mt-22'>
+      <h2 className='font-bold text-6xl font-game'>Start Your</h2>
+      <h2 className='font-bold text-7xl font-game text-yellow-400'
+      style={{
+        textShadow: '2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 0px 2px 0 #000, 2px 0px 0 #000, 0px -2px 0 #000, -2px 0px 0 #000'
+  }}>Coding Journey</h2>
+  <h2 className='text-xl text-gray-300 font-game'>Beginner friendly courses and projects</h2>
+      <Button className='mt-6 font-game text-2xl ' variant={'pixel'}>Get Started</Button>
+     </div>
     </div>
   )
 }
